a01e7dfe67c3ca6ba8bbb80c000e7b05405d9b77
bddppq <bai@in.tum.de>
2018-02-27 23:37:19 -0800

    Revert "[wip] Cmake modernization (#2066)"
    This reverts commit 6a77431476a2ea24d407805ec902bf1f632d3ca9.

--- cmake/MiscCheck.cmake
+++ cmake/MiscCheck.cmake
@@ -1,4 +1,3 @@
-include(CheckCCompilerFlag)
 include(CheckCXXSourceCompiles)
 include(CheckCXXCompilerFlag)
 include(CMakePushCheckState)
@@ -171,17 +170,6 @@ endif()
 if (IOS)
   add_definitions("-mfpu=neon-fp16")
   add_definitions("-Wno-deprecated-declarations")
-endif()
-
-# ---[ If we are building with ACL, we will enable neon-fp16.
-if(USE_ACL)
-  if(NOT USE_ARM64)
-    add_definitions("-mfpu=neon-fp16")
-  endif()
-  CHECK_C_COMPILER_FLAG(-mfp16-format=ieee CAFFE2_COMPILER_SUPPORTS_FP16_FORMAT)
-  if(CAFFE2_COMPILER_SUPPORTS_FP16_FORMAT)
-    add_definitions("-mfp16-format=ieee")
-  endif()
 endif()
 
 # ---[ If we use asan, turn on the flags.
